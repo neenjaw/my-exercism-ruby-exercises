93a72cc0371befa208bece03069a1f62fd41ebd3
Tim Austin <tim@neenjaw.com>
2020-07-06 18:54:04 -0600

    preferential solution

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -2,22 +2,22 @@
 
 # module for working with DNA/RNA strands
 module Complement
+  def self.of_dna(strand)
+    strand.tr('GCTA', 'CGAU')
+  end
+
   # def self.of_dna(strand)
-  #   strand.tr('GCTA', 'CGAU')
+  #   strand.chars.map { |c| translate(c) }.join
   # end
 
-  def self.of_dna(strand)
-    strand.chars.map { |c| translate(c) }.join
-  end
+  # private_class_method def self.translate(char)
+  #   PAIRS[char] || char
+  # end
 
-  private_class_method def self.translate(char)
-    PAIRS[char] || char
-  end
-
-  private_constant PAIRS = {
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
-  }.freeze
+  # private_constant PAIRS = {
+  #   'G' => 'C',
+  #   'C' => 'G',
+  #   'T' => 'A',
+  #   'A' => 'U'
+  # }.freeze
 end
